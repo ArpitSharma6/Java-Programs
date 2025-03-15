04a62f46ac1cd5d52bebfb1a22c16f1e92e65d4b
Arpit Sharma <arpit.sharma71195@gmail.com>
2025-03-15 21:33:50 +0530

    1st code

---
+++ Asgn_1PrintName.java
@@ -0,0 +1,11 @@
+package Batch51JavaPrograms;
+
+public class Asgn_1PrintName {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Arpit Sharma");
+	}
+
+}
